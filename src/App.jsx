7bd0d673da7f18fa7a32cb4fa8386a6f9f61d719
bwlee <lbw3973@naver.com>
2023-03-28 14:38:09 +0900

    Feat: App Container 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,54 @@
-import { useState } from "react";
-import "./App.css";
+import styled from "@emotion/styled";
+import { Provider } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import AllLocation from "./pages/AllLocation/AllLocation";
+import Favorite from "./pages/Favorite/Favorite";
+import MyLocation from "./pages/MyLocation/MyLocation";
+import { store } from "./store";
+
+const Container = styled.div`
+  width: 720px;
+  height: 95vh;
+  min-width: 500px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px auto;
+
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  position: relative;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px 0;
+  gap: 30px;
+  width: 100%;
+  height: 74vh;
+  overflow-y: auto;
+`;
 
 function App() {
-  return <div></div>;
+  return (
+    <Provider store={store}>
+      <Container>
+        <Wrapper>
+          <Routes>
+            <Route path="/" element={<MyLocation />} />
+            <Route path="/myLocation" element={<MyLocation />} />
+            <Route path="/allLocation" element={<AllLocation />} />
+            <Route path="/favorite" element={<Favorite />} />
+          </Routes>
+        </Wrapper>
+        <Navigation />
+      </Container>
+    </Provider>
+  );
 }
 
 export default App;
