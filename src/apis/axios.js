e8ff94a0f05005edf4acc6c2446a06ed1839aae3
bwlee <lbw3973@naver.com>
2023-03-28 14:33:28 +0900

    Feat: api 설정 완료

---
+++ src/apis/axios.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const getParameters = {
+  serviceKey: `${import.meta.env.VITE_API_KEY}`,
+  returnType: "json",
+  numOfRows: "100",
+  pageNo: "1",
+  sidoName: "서울",
+  ver: "1.0",
+};
+
+export const fetchData = async (param) => {
+  const response = await axios.get(
+    "/api/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty",
+    { params: param }
+  );
+  return response.data.response.body.items;
+};
