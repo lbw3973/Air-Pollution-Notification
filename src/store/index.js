a7e57635b26375b6f882a0f5272e88f9e60d9eda
bwlee <lbw3973@naver.com>
2023-03-28 22:30:47 +0900

    Feat: persist Reducer  store 구현 완료

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,27 @@ import {
   combineReducers,
   legacy_createStore as CreateStore,
 } from "redux";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
 import ReduxThunk from "redux-thunk";
-import { getStationReducer } from "../reducers/GetStationInfo";
+import { getFavoriteDataReducer } from "../reducers/getFavoriteData";
+import { getPollutionReducer } from "../reducers/getPollutionData";
+import { getStationDataReducer } from "../reducers/getStationData";
 
-const rootReducer = getStationReducer;
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  favorite: ["getFavoriteDataReducer"],
+};
 
-export const store = CreateStore(rootReducer, applyMiddleware(ReduxThunk));
+const rootReducer = combineReducers({
+  getPollutionReducer,
+  getStationDataReducer,
+  getFavoriteDataReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = CreateStore(persistedReducer, applyMiddleware(ReduxThunk));
+export const persistor = persistStore(store);
