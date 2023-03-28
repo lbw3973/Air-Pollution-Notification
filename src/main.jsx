168528e6684ba80ad27af45ecd3e8e05c0ba1fb7
bwlee <lbw3973@naver.com>
2023-03-28 14:33:55 +0900

    Feat: Style 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import GlobalStyle from "./styles/Global";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
