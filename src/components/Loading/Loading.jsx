1dcaba98616787d1f35fc1b33b5fa4486a89279e
bwlee <lbw3973@naver.com>
2023-08-23 15:15:21 +0900

    Feat: Loading 컴포넌트 추가

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./Loading.style";
+
+function Loading() {
+  return (
+    <S.Container>
+      <img src="src/assets/loading.gif" />
+    </S.Container>
+  );
+}
+
+export default Loading;
