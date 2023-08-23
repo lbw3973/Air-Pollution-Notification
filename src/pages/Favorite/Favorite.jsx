2f0875a76631a3bf754c69b8385e08269c85bf60
bwlee <lbw3973@naver.com>
2023-08-23 15:17:41 +0900

    Feat: 즐겨찾기 목록 비어있을 시 문구 추가

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import LocalInfo from "../../components/LocalInfo/LocalInfo";
-import * as S from "./Favorite.style";
 
 const Favorite = () => {
   const favorites = useSelector((state) => state.getFavoriteDataReducer);
   const favor_stationName = favorites.map((data) => data.stationName);
+
+  if (!favorites.length) return <>즐겨찾기 목록이 비어있습니다.</>;
   return (
     <>
       {favorites &&
